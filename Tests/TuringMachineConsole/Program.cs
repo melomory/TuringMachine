19845d392a0bad896097764bc8b63ae01353a565
Melomory <niko.d.v@yandex.ru>
2021-09-01 19:37:06 +0400

    Request to a remote server to get raw data

--- Tests/TuringMachineConsole/Program.cs
+++ Tests/TuringMachineConsole/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 
 namespace TuringMachineConsole
 {
     class Program
     {
+        const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //WebClient client = new WebClient();
+
+            HttpClient httpClient = new();
+
+            HttpResponseMessage response = httpClient.GetAsync(data_url).Result;
+            string stCSV = response.Content.ReadAsStringAsync().Result;
+
+            Console.ReadLine();
         }
     }
 }
