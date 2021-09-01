aaa9cddfa9592e9588ce603b035551cfe5276d85
Melomory <niko.d.v@yandex.ru>
2021-09-01 20:16:10 +0400

    Retrieving data by a recording date

--- Tests/TuringMachineConsole/Program.cs
+++ Tests/TuringMachineConsole/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
-using System.Net;
+using System.Linq;
 using System.Net.Http;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
 
 namespace TuringMachineConsole
 {
@@ -8,14 +14,53 @@ namespace TuringMachineConsole
     {
         const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+        private static async Task<Stream> GetDataStream()
+        {
+            HttpClient httpClient = new();
+            HttpResponseMessage response = await httpClient.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var dataStream = GetDataStream().Result;
+            using var dataReader = new StreamReader(dataStream);
+
+            while (!dataReader.EndOfStream)
+            {
+                var line = dataReader.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    continue;
+                }
+                yield return line;
+            }
+        }
+
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+            .ToArray();
+
         static void Main(string[] args)
         {
             //WebClient client = new WebClient();
 
-            HttpClient httpClient = new();
+            //HttpClient httpClient = new();
 
-            HttpResponseMessage response = httpClient.GetAsync(data_url).Result;
-            string stCSV = response.Content.ReadAsStringAsync().Result;
+            //HttpResponseMessage response = httpClient.GetAsync(data_url).Result;
+            //string stCSV = response.Content.ReadAsStringAsync().Result;
+
+
+            //foreach (var data_line in GetDataLines())
+            //{
+            //    Console.WriteLine(data_line);
+            //}
+
+            var dates = GetDates();
+            Console.WriteLine(String.Join("\r\n",dates));
 
             Console.ReadLine();
         }
