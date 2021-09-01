b9a388794cbacc498923d849e7c1050df610a872
Melomory <niko.d.v@yandex.ru>
2021-09-01 20:48:02 +0400

    Test business logic

--- Tests/TuringMachineConsole/Program.cs
+++ Tests/TuringMachineConsole/Program.cs
@@ -33,7 +33,7 @@ namespace TuringMachineConsole
                 {
                     continue;
                 }
-                yield return line;
+                yield return line.Replace("Korea,", "Korea -");
             }
         }
 
@@ -43,6 +43,25 @@ namespace TuringMachineConsole
             .Skip(4)
             .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
             .ToArray();
+
+
+        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
+        {
+            var lines = GetDataLines()
+                .Skip(1)
+                .Select(line => line.Split(','));
+
+            foreach (var row in lines)
+            {
+                var provinceName = row[0].Trim();
+                var countryName = row[1].Trim(' ', '"');
+                var counts = row.Skip(5)
+                    .Select(int.Parse)
+                    .ToArray();
+                yield return (countryName, provinceName, counts);
+            }
+
+        }
 
         static void Main(string[] args)
         {
@@ -59,8 +78,13 @@ namespace TuringMachineConsole
             //    Console.WriteLine(data_line);
             //}
 
-            var dates = GetDates();
-            Console.WriteLine(String.Join("\r\n",dates));
+            //var dates = GetDates();
+            //Console.WriteLine(String.Join("\r\n",dates));
+
+
+            var russiaData = GetData().First(v => v.Country.Equals("Russia", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine(String.Join("\r\n", GetDates().Zip(russiaData.Counts, (date, count) => $"{date:dd.MM} – {count}")));
 
             Console.ReadLine();
         }
